20370dee1f960fba9bd1b8ebbb03ac4d8a58700d
kparekh01 <kparekh01@gmail.com>
2016-04-24 13:41:19 +0000

    nitrous-backup at Sun Apr 24 13:41:19 UTC 2016

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
 # your code goes here
+def begins_with_r(array)
+  array.all?{|words| words.start_with?("r")}
+end
+
+def contain_a(array)
+  array.select{|word| word.include?("a")}
+end
+
+def first_wa(array)
+  array.find{|word| word.class == String && word.start_with?("wa")}
+end
+
+def remove_non_strings(array)
+  array.delete_if{|word| word.class != String}
+end
